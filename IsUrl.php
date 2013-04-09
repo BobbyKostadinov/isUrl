6e9ac07252f2529652525fa660fbeb263bf3d356
Bobby Kostadinov <b.a.kostadinov@gmail.com>
2013-04-09 13:03:34 +0100

    add class

---
+++ IsUrl.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Validate query strings
+ * @author Bobby Kostadinov <b.a.kostadinov@gmail.com>
+ * 
+ * Usage: Just place this class in your /library/Validate and take advantage of this custom validator
+ *
+ */
+class Validate_IsUrl extends Zend_Validate_Abstract
+{
+
+    const INVALID_URL = 'invalidUrl';
+
+    protected $_messageTemplates = array(
+        self::EXCEPTION => "'%value%' is not a valid URL",
+        self::INVALID_URI => "'%value%' is not a valid URL. Invalid string provided.",
+    );
+
+    public function isValid($value)
+    {
+        if (!is_string($value)) {
+            $this->_error(self::INVALID_URL);
+             return false;
+        }
+
+        $this->_setValue($value);
+        
+        try {
+            //Zend_Uri will result in exception if schema is invalid
+            $uriHttp = Zend_Uri_Http::fromString($value);
+        } catch (Zend_Uri_Exception $e) {
+            $this->_error(self::EXCEPTION);
+            return false;
+        }
+        
+        //Perform validation on all parts of the URL - host, query string etc
+        if (false === $uriHttp->valid()) {
+            $this->_error(self::INVALID_URL);
+            return false;
+        }
+       
+        return true;
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,10 @@ isUrl
 =====
 
 Zend Framework 1 Validator for URLs
+
+Usage:
+Place this class inside your included path (library/Validate/isUrl.php).
+
+This class is using Zend_Uri to validate strings as URLs. 
+
+It will make sure that the scheme is valid, the hostname is real and that the query string is a valid URL string
